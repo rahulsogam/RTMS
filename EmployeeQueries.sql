3a69d3c47ba9b251fc8bae00546a7c067c3b84c0
unknown <sarveshbhosale111@gmail.com>
2022-12-28 19:20:32 +0000

    function

--- EmployeeQueries.sql
+++ EmployeeQueries.sql
@@ -1,4 +1,5 @@
 --Tables
+
 CREATE TABLE Designation(
 	designationID int NOT NULL PRIMARY KEY IDENTITY(1,1),
 	designationName varchar(255),
@@ -22,6 +23,16 @@ Create Table Reciepes(
 	ingredients xml,
 	steps xml
 );
+
+Create Table Attendance(
+	attendanceID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	attendanceDate date NOT NULL DEFAULT (GETDATE()),
+	employeeID int FOREIGN KEY REFERENCES Employee(employeeID),
+	attendanceStatus varchar(1),
+	punchInTime time,
+	punchOutTime time,
+	totalHours int
+)
 
 --Insert Data from XML file to table
 insert into Reciepes
@@ -53,22 +64,31 @@ CREATE or ALTER PROCEDURE addEmployee @firstName varchar(255), @lastName varchar
 AS
 insert into Employee(firstName, lastName, address, city, designationID , phoneNo) values (@firstName,@lastName,@address,@city,@designationID, @phoneNo);
 
+exec addEmployee @firstName = 'Sarvesh' , @lastName = 'Bhosale', @address = 'The Hardwicke', @city = 'Dublin', @designationID = 1, @phoneNo = 0899756487
+
 CREATE or ALTER PROCEDURE getEmployees
 AS
 Select employeeID, firstName,lastName,address,city,phoneNo,designationName from Employee_Details;
+
+exec getEmployees
 
 Create or Alter procedure getEmployeeByID @id int
 AS
 Select * from Employee_Details where employeeID = @id
 
+exec getEmployeeByID @id=1
+
 CREATE or ALTER PROCEDURE updateEmployee @employeeID int, @firstName varchar(255), @lastName varchar(255), @address varchar(255), @city varchar(255), @phoneNo varchar(10)
 AS
 update Employee set firstName=@firstName, lastName=@lastName, address=@address, city=@city, phoneNo= @phoneNo  where employeeID = @employeeID;
+
+exec updateEmployee @employeeID=8, @firstName = 'Sarvesh' , @lastName = 'Bhosale', @address = 'The Hardwicke', @city = 'Dublin', @phoneNo = 0899756487
 
 CREATE or ALTER PROCEDURE deleteEmployee @employeeID int
 AS
 delete from Employee where employeeID=@employeeID
 
+exec deleteEmployee @employeeID = 8
 
 --Designation Section
 
@@ -76,19 +96,25 @@ CREATE or ALTER PROCEDURE addDesignation @designationName varchar(255), @designa
 AS
 insert into Designation(designationName, designationSalary) values (@designationName,@designationSalary);
 
+exec addDesignation @designationName = 'Manager', @designationSalary = 20000
+
 CREATE or ALTER PROCEDURE getDesignation
 AS
 Select * from Designation;
+
+exec getDesignation 
 
 CREATE or ALTER PROCEDURE changeSalary @designationID int, @designationSalary money
 AS
 update Designation set designationSalary=@designationSalary where designationID = @designationID;
 
+exec changeSalary @designationID = 1, @designationSalary = 20000
 
 CREATE or ALTER PROCEDURE deleteDesignation @designationID int
 AS
 delete from Designation where designationID=@designationID;
 
+exec deleteDesignation @designationID=3
 
 --Reciepes
 
@@ -103,3 +129,44 @@ AS
 Select r.reciepeName,r.employeeID,r.ingredients.query('ingredient').value('.','varchar(500)') as ingridients, r.steps.query('step').value('.','varchar(500)') as steps from Reciepes r;
 
 exec getReciepes;
+
+
+--Attendance
+
+CREATE or ALTER PROCEDURE addPunchin @employeeID int 
+AS
+Insert into Attendance(employeeID,attendanceStatus,punchInTime) values( @employeeID,'p',GETDATE());
+
+exec addPunchin @employeeID = 3;
+
+
+Create or Alter Procedure addPunchout @attendanceID int
+As
+update Attendance set punchOutTime = GETDATE() where attendanceID = @attendanceID;
+
+exec addPunchout @attendanceID = 3;
+
+Create or Alter Trigger punchout on Attendance
+AFTER Update 
+AS
+declare @punchInTime time
+declare @punchOutTime time
+declare @attendanceID int
+
+select @attendanceID = attendanceID from inserted;
+select @punchOutTime = punchOutTime from inserted;
+select @punchInTime = punchInTime from inserted;
+print('reached')
+update Attendance set totalHours = dbo.getTotalHours(@punchInTime,@punchOutTime) where attendanceID = @attendanceID
+
+
+
+
+Create or Alter function getTotalHours(@punchInTime as time,@punchOutTime as time)
+returns int
+AS
+BEGIN
+	DECLARE @totalHours as int
+	set @totalHours = DateDiff(hour, @punchInTime, @punchOutTime)
+	return @totalHours
+END
