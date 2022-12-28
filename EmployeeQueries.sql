b98dbb81b639f9d5eccf1a396cd68d71a9c26b28
unknown <sarveshbhosale111@gmail.com>
2022-12-28 02:12:06 +0000

    Employee Curd View and Join

--- EmployeeQueries.sql
+++ EmployeeQueries.sql
@@ -1,3 +1,4 @@
+--Tables
 CREATE TABLE Designation(
 	designationID int NOT NULL PRIMARY KEY IDENTITY(1,1),
 	designationName varchar(255),
@@ -10,15 +11,48 @@ CREATE TABLE Employee (
     firstName varchar(255),
     address varchar(255),
     city varchar(255),
+	phoneNo varchar(10),
 	designationID int FOREIGN KEY REFERENCES Designation(designationID),                                                                                                                                                                  
 );
 
+--Views
+
+Create or Alter View Employee_Details as Select e.employeeID,e.firstName, e.lastName, e.address,e.city ,e.phoneNo,d.designationSalary, d.designationName from Employee e INNER JOIN Designation d ON e.designationID = d.designationID;
+
+
+
+--Employee Section
+
+CREATE or ALTER PROCEDURE addEmployee @firstName varchar(255), @lastName varchar(255), @address varchar(255), @city varchar(255), @designationID int, @phoneNo varchar(10)
+AS
+insert into Employee(firstName, lastName, address, city, designationID , phoneNo) values (@firstName,@lastName,@address,@city,@designationID, @phoneNo);
+
+CREATE or ALTER PROCEDURE getEmployees
+AS
+Select employeeID, firstName,lastName,address,city,phoneNo,designationName from Employee_Details;
+
+Create or Alter procedure getEmployeeByID @id int
+AS
+Select * from Employee_Details where employeeID = @id
+
+CREATE or ALTER PROCEDURE updateEmployee @employeeID int, @firstName varchar(255), @lastName varchar(255), @address varchar(255), @city varchar(255), @phoneNo varchar(10)
+AS
+update Employee set firstName=@firstName, lastName=@lastName, address=@address, city=@city, phoneNo= @phoneNo  where employeeID = @employeeID;
+
+CREATE or ALTER PROCEDURE deleteEmployee @employeeID int
+AS
+delete from Employee where employeeID=@employeeID
+
 
 --Designation Section
+
 CREATE or ALTER PROCEDURE addDesignation @designationName varchar(255), @designationSalary money
 AS
 insert into Designation(designationName, designationSalary) values (@designationName,@designationSalary);
 
+CREATE or ALTER PROCEDURE getDesignation
+AS
+Select * from Designation;
 
 CREATE or ALTER PROCEDURE changeSalary @designationID int, @designationSalary money
 AS
--- src/main/java/com/rtms/service/EmployeeService.java
+++ src/main/java/com/rtms/service/EmployeeService.java
@@ -28,6 +28,148 @@ public class EmployeeService {
 	CallableStatement cs;
 	Connection connection;
 	
+	//Employee
+	
+	public JSONObject addEmployee(String firstName, String lastName, String address, String city, String designationID, String phoneNo) {
+		data = new JSONArray() ;
+		emp = new JSONObject();
+		try {
+			connection= dataSource.getConnection();
+			cs = connection.prepareCall("{call addEmployee(?,?,?,?,?,?)}");
+			cs.setString(1, firstName);
+			cs.setString(2, lastName);
+			cs.setString(3, address);
+			cs.setString(4, city);
+			cs.setString(5, designationID);
+			cs.setString(6, phoneNo);
+			
+			int a=cs.executeUpdate();
+			System.out.println(a);
+			if(a!=0) {
+				emp.put("msg", "Success");
+				}
+			}
+			catch(Exception e) {
+				emp.put("Exception", e.toString());
+				e.printStackTrace();
+			}
+		System.out.println(emp);
+		return emp;
+	}
+	
+	public JSONObject getEmployees() {
+		data = new JSONArray() ;
+		emp = new JSONObject();
+		try {
+			connection= dataSource.getConnection();
+			cs = connection.prepareCall("{call getEmployees()}");
+			rs = cs.executeQuery();
+			while(rs.next()) {
+				emp.put("msg", "Success");
+				JSONObject result= new JSONObject();
+				result.put("ID",rs.getString("employeeID"));
+				result.put("firstName",rs.getString("firstName"));
+				result.put("LastName", rs.getString("LastName"));
+				result.put("address", rs.getString("address"));
+				result.put("city", rs.getString("city"));
+				result.put("phoneNo", rs.getString("phoneNo"));
+				result.put("designationName", rs.getString("designationName"));
+				data.put(result);
+				}
+			emp.put("Employees", data);
+			
+			}
+			catch(Exception e) {
+				emp.put("Exception", e.toString());
+				e.printStackTrace();
+			}
+		System.out.println(emp);
+		return emp;	
+	}
+	
+	public JSONObject getEmployeeByID(String id) {
+		data = new JSONArray() ;
+		emp = new JSONObject();
+		try {
+			connection= dataSource.getConnection();
+			cs = connection.prepareCall("{call getEmployeeByID(?)}");
+			cs.setString(1, id);
+			rs = cs.executeQuery();
+			while(rs.next()) {
+				emp.put("msg", "Success");
+				JSONObject result= new JSONObject();
+				result.put("ID",rs.getString("employeeID"));
+				result.put("firstName",rs.getString("firstName"));
+				result.put("LastName", rs.getString("LastName"));
+				result.put("address", rs.getString("address"));
+				result.put("city", rs.getString("city"));
+				result.put("phoneNo", rs.getString("phoneNo"));
+				result.put("designationName", rs.getString("designationName"));
+				result.put("Salary",rs.getString("designationSalary"));
+				emp.put("Employee", result);
+				}
+			}
+			catch(Exception e) {
+				emp.put("Exception", e.toString());
+				e.printStackTrace();
+			}
+		System.out.println(emp);
+		return emp;	
+	}
+	
+	public JSONObject updateEmployee(String employeeID,String firstName, String lastName, String address, String city,String phoneNo) {
+		data = new JSONArray() ;
+		emp = new JSONObject();
+
+		try {
+			connection= dataSource.getConnection();
+			cs = connection.prepareCall("{call updateEmployee(?,?,?,?,?,?)}");
+			cs.setString(1, employeeID);
+			cs.setString(2, firstName);
+			cs.setString(3, lastName);
+			cs.setString(4, address);
+			cs.setString(5, city);
+			cs.setString(6, phoneNo);
+			
+			int a=cs.executeUpdate();
+			System.out.println(a);
+			if(a!=0) {
+				emp.put("msg", "Success");
+				}
+			}
+			catch(Exception e) {
+				emp.put("Exception", e.toString());
+				e.printStackTrace();
+			}
+		System.out.println(emp);
+		return emp;
+	}
+	
+	public JSONObject deleteEmployee(String employeeID) {
+		data = new JSONArray() ;
+		emp = new JSONObject();
+		try {
+			connection= dataSource.getConnection();
+			cs = connection.prepareCall("{call deleteEmployee(?)}");
+			cs.setString(1, employeeID);
+			int a=cs.executeUpdate();
+			System.out.println(employeeID);
+			if(a!=0) {
+				emp.put("msg", "Success");
+				}
+			}
+			catch(Exception e) {
+				emp.put("Exception", e.toString());
+				e.printStackTrace();
+			}
+		System.out.println(emp);
+		return emp;
+	}
+	
+	
+	
+	//Designation
+	
 	public JSONObject addDesignation(String designationName, String designationSalary) {
 		System.out.println(designationSalary);
 		data = new JSONArray() ;
@@ -92,6 +234,32 @@ public class EmployeeService {
 			}
 		System.out.println(emp);
 		return emp;
+	}
+	
+	public JSONObject getDesignation() {
+		data = new JSONArray() ;
+		emp = new JSONObject();
+		try {
+			connection= dataSource.getConnection();
+			cs = connection.prepareCall("{call getDesignation()}");
+			rs = cs.executeQuery();
+			while(rs.next()) {
+				emp.put("msg", "Success");
+				JSONObject result= new JSONObject();
+				result.put("designationID",rs.getString("designationID"));
+				result.put("designationName", rs.getString("designationName"));
+				result.put("designationSalary", rs.getString("designationSalary"));
+				data.put(result);
+				}
+			emp.put("Designations", data);
+			
+			}
+			catch(Exception e) {
+				emp.put("Exception", e.toString());
+				e.printStackTrace();
+			}
+		System.out.println(emp);
+		return emp;	
 	}
 
 }
